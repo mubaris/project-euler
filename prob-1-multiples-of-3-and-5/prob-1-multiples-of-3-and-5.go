944d1639bc25d52a8f3a7fda6e1412d216fdb1bc
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-03 23:58:11 +0530

    Modify Solution 1 - Optimized Solution in Go

--- prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.go
+++ prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.go
@@ -2,13 +2,23 @@ package main
 
 import . "fmt"
 
+func sumDivisibleBy(n int) int {
+	p := 999 / n
+	return int(n * p * (p + 1) / 2)
+}
+
 func main() {
 	total := 0
 
-	for i := 1; i < 1000; i++ {
+	/*for i := 1; i < 1000; i++ {
 		if (i%3 == 0) || (i%5 == 0) {
 			total += i
 		}
-	}
+	}*/
+
+	//Optimized Solution
+
+	total = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15)
+
 	Println(total)
 }
