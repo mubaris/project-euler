bc1440662698f2828acba2101918b49e7a283fad
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-04 00:22:45 +0530

    Modify Solution 1 - Optimized Version in Python3

--- prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.py
+++ prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.py
@@ -1,7 +1,14 @@
 total = 0
 
-for i in range(1000):
+'''for i in range(1000):
     if i % 3 == 0 or i % 5 == 0:
-        total += i
+        total += i'''
+#Optimized Solution
+
+def sumDivisibleBy(n):
+    p = int(999 / n)
+    return int(n * p * (p + 1) / 2)
+
+total = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15)
 
 print(total)
