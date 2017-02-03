181619d03691fa655a7d165658076b702a365c2f
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-03 23:42:34 +0530

    Modify Solution 1 - Optimized Version in C

--- prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.c
+++ prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.c
@@ -2,12 +2,25 @@
 
 int main() {
 	int total = 0;
-	int i = 0;
+	//int i = 0;
 
-	for(i = 0; i < 1000; i++) {
+	/*for(i = 0; i < 1000; i++) {
 		if((i % 3 == 0) || (i % 5 == 0)) {
 			total += i;
 		}
+	}*/
+
+	//Optimized Solution
+	
+	int target = 999;
+
+	int sumDivisibleBy(int n) {
+		int p;
+		p = target / n;
+		return (n * p * (p + 1)) / 2;
 	}
+
+	total = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
+	
 	printf("%d\n", total);
 }
