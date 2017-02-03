a57a87f22fb16881085221e2f058d00589e20a45
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-04 00:15:33 +0530

    Modify Solution 1 - Optimized Code in JS/Node.js

--- prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.js
+++ prob-1-multiples-of-3-and-5/prob-1-multiples-of-3-and-5.js
@@ -1,9 +1,18 @@
 var total = 0;
 
-for(var i = 0; i < 1000; i++) {
+/*for(var i = 0; i < 1000; i++) {
 	if((i % 3 == 0) || (i % 5 == 0)) {
 		total += i;
 	}
+}*/
+
+//Optimized Solution
+
+function sumDivisibleBy(n) {
+	var p = parseInt(999 / n, 10)
+	return parseInt(n * p * (p + 1) / 2, 10)
 }
+
+total = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15)
 
 console.log(total)
