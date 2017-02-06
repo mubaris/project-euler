ea4c8ba06fa3e410734d09a7272e1c7708d289c8
Mubaris Hassan <mubarishassannk@gmail.com>
2017-02-06 19:27:33 +0530

    Add Solution 7 in Go

---
+++ prob-7-10001st-prime/prob-7-10001st-prime.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func isPrime(n int64) int {
+	for i := int64(2); i*i <= n; i++ {
+		if n%i == int64(0) {
+			return 0
+		}
+	}
+	return 1
+}
+
+func prime(n int64) int64 {
+	var counter int64 = 0
+	if n == 1 {
+		return 2
+	}
+	for i := int64(3); ; i++ {
+		if isPrime(i) == 1 {
+			counter++
+			if counter == n-1 {
+				return i
+			}
+		}
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(prime(10001))
+}
